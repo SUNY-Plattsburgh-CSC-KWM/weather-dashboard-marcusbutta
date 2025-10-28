cb231005e2ff9c085d98839f54d2d4feae010318
Marcus <me@marcusbutta.com>
2025-10-28 15:09:18 -0400

    Setup basic site with coordinate input

---
+++ main.js
@@ -0,0 +1,9 @@
+let latitude;
+let longitude;
+
+function getCords() {
+    latitude = document.getElementById("latitude").value;
+    longitude = document.getElementById("longitude").value;
+    console.log(latitude);
+    console.log(longitude);
+}
