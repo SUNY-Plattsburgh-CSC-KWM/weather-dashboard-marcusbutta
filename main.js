3f17bee26cd2f88760e3b72a32500f5c8aaaf98b
Marcus <me@marcusbutta.com>
2025-10-30 14:56:59 -0400

    Add forecast buttons and unit toggle

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 let latitude;
 let longitude;
+let days;
+let unit;
 
-function getCords() {
+function getForecast() {
     latitude = document.getElementById("latitude").value;
     longitude = document.getElementById("longitude").value;
+    days = document.getElementById("forecast-days").value;
     console.log(latitude);
     console.log(longitude);
+    console.log(days);
+}
+
+function changeUnit() {
+    unit = document.getElementById("unit");
+    if (unit.innerText === "C°") {
+        unit.innerText = "F°";
+    } else {
+        unit.innerText = "C°";
+    }
 }
